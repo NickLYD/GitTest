5af6547086fb87c1880d6869a9d692d42d95c63f
luyangdong <luyangdong@apexsoft.com.cn>
2020-09-04 09:51:48 +0800

    7

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,10 +7,6 @@ public class Test {
     private int b;
     public Test(){}
     public void fA(){
-        System.out.println("Hello");
-        System.out.println("Hello World!");
-    }
-    public void fD(){
         System.out.println("Hello World!");
     }
     public void fB(){}
