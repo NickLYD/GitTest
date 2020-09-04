608fd59f0857712f73e16c012613048befb4c111
luyangdong <luyangdong@apexsoft.com.cn>
2020-09-04 09:11:35 +0800

    1

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @Author Administrator
+ * @Date 2020/9/4 9:08
+ */
+public class Test {
+    private int a;
+    private int b;
+    public Test(){}
+    public void fA(){
+        System.out.println("Hello World!");
+    }
+    public void fB(){}
+}
